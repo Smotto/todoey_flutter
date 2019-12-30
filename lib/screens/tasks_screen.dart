7d4fc5189dd72c1d5baa9575ff7b46209371fc25
Jarod Castillo <Jarodc2017@gmail.com>
2019-12-30 14:52:38 -0800

    todoey icon

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,9 +5,24 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(
+      body: Container(
+        padding:
+            EdgeInsets.only(left: 30.0, top: 60.0, right: 30.0, bottom: 30.0),
         child: Column(
-          children: <Widget>[Icon(Icons.list), Text('Todoey')],
+          children: <Widget>[
+            CircleAvatar(
+              child:
+                  Icon(Icons.list, size: 30.0, color: Colors.lightBlueAccent),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            SizedBox(height: 10.0),
+            Text('Todoey',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold)),
+          ],
         ),
       ),
     );
